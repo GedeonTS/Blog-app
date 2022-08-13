fc74e87ea82b18f1895fd75a1d85f7e8e8b9da51
GedeonTS <gedeontshobohwa1@gmail.com>
2022-08-13 15:39:52 +0200

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :Name, presence: true
+  validates :PostsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   has_many :comments, foreign_key: 'AuthorId'
   has_many :likes, foreign_key: 'AuthorId'
   has_many :posts, foreign_key: 'AuthorId'
