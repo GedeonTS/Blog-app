e07cf7312d1008c10c3b5faa5ec7c863d5393b8c
GedeonTS <gedeontshobohwa1@gmail.com>
2022-08-20 00:24:48 +0200

    Update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+  end
 end
