ce3bcb6c4818c7996c8b12dd781286aeda5f550d
GedeonTS <gedeontshobohwa1@gmail.com>
2022-08-24 00:21:39 +0200

    Refactor controllers test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  user = User.create(Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Mexico.',
-                     PostsCounter: 0)
-  post = Post.create(AuthorId: user, Title: 'Hello', Text: 'This is my first post', CommentsCounter: 1, LikesCounter: 1)
+  user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+                     posts_counter: 0)
+  post = Post.create(author_id: user, title: 'Hello', text: 'This is my first post', comments_count: 1)
 
-  subject { Comment.new(posts_id: post, AuthorId: user, Text: 'this is where the comment text goes') }
+  subject { Comment.new(post_id: post, author_id: user, text: 'this is where the comment text goes') }
 
   before { subject.save }
 
   it 'comments counter should not be nil' do
-    expect(post.CommentsCounter).to eq 1
+    expect(post.comments_count).to eq 1
   end
 end
